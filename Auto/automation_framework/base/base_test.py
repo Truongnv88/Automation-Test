55e075b6c9dee5be7c9e30a6aabea35fada23b1e
Truongnv88 <nvantruong.work@gmail.com>
2025-09-11 15:39:00 +0700

    Update LoginPage with POM code

---
+++ Auto/automation_framework/base/base_test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import pytest
+from time import sleep
+from selenium.webdriver.common.by import By
+
+class BaseTest:
+    @pytest.fixture(scope="class", autouse= True)
+    def setup(self, request):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        sleep(10)
+        request.cls.driver=self.driver
+        yield #dừng
+        self.driver.quit()
