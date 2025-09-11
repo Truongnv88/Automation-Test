b9f223d2563e0cec0709427f8111af0e457fc619
Truongnv88 <nvantruong.work@gmail.com>
2025-09-11 20:48:24 +0700

    commit code login

--- Auto/automation_framework/pages/login_page.py
+++ Auto/automation_framework/pages/login_page.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.by import By
-from base.base_test import BaseTest
 
-class LoginPage(BaseTest):
+class LoginPage:
+   def __init__(self, driver):
+      self.driver = driver
    def login(self, username, password):
-    self.driver.find_element(By.XPATH, "//input[@name='username']").send_keys(username) #username
+    self.driver.find_element(By.XPATH, "//input[@name='username']").send_keys(username)
     self.driver.find_element(By.XPATH, "//input[@name='password']").send_keys(password)
     self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
